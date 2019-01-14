9a1a2701831ce64e6d9ebc1568787aa6e77e841e
xarkos <gordon_leek@yahoo.com>
2019-01-14 12:08:17 +0000

    Whistling into Hackney and True

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,53 @@
 
 
- document.addEventListener('deviceready', this.onDeviceReady, false);
+ //document.addEventListener('deviceready', this.onDeviceReady, false);
 
- function onDeviceReady() {
+// function onDeviceReady() {
 
-  
+//}
 
- }
+
+
+// $(window).on("load",function(){
+// You can write the code here as well
+// });
+ 
+ 
+$("#foo").on("pagecreate",function(event){ //can use pagebeforecreate as well
+ 
+    function adSetter(){
+    alert(navigator.userAgent);
+    var admobid = {};
+    // select the right Ad Id according to platform
+    if( /(android)/i.test(navigator.userAgent) ) { 
+        admobid = { // for Android
+            banner: 'ca-app-pub-3940256099942544/6300978111',
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+    } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+        admobid = { // for iOS
+            banner: 'ca-app-pub-3940256099942544/6300978111',
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+    } else {
+        admobid = { // for Windows Phone
+            banner: 'ca-app-pub-3940256099942544/6300978111',
+            interstitial: 'ca-app-pub-3940256099942544/1033173712'
+        };
+    }
+     
+    if(AdMob) AdMob.createBanner( {
+        adId:admobid.banner, 
+        position:AdMob.AD_POSITION.BOTTOM_CENTER, 
+        autoShow:true} );
+     
+    }
+     
+        function onDeviceReady(){
+         alert("device ready");
+          adSetter();
+      }
+     
+      document.addEventListener("deviceready", onDeviceReady, false);
+     
+    });
